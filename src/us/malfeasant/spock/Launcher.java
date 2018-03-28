d1af958106c77548d72766eb3fbe31960bd4b340
Malfeasant <mischasumrall@gmail.com>
2018-03-28 16:32:15 -0700

    Added JavaFX cruft

---
+++ src/us/malfeasant/spock/Launcher.java
@@ -0,0 +1,20 @@
+package us.malfeasant.spock;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		BorderPane root = new BorderPane();
+		Scene scene = new Scene(root,400,400);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
