15bc389db79cc00c5d7775985ebf29f59f29fc95
Malfeasant <mischasumrall@gmail.com>
2018-03-28 16:52:18 -0700

    Couple buttons...

--- src/us/malfeasant/spock/Launcher.java
+++ src/us/malfeasant/spock/Launcher.java
@@ -2,14 +2,26 @@ package us.malfeasant.spock;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Launcher extends Application {
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		BorderPane root = new BorderPane();
-		Scene scene = new Scene(root,400,400);
+	public void start(Stage primaryStage) {
+		Button startNew = new Button("New Design");
+		startNew.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+		startNew.setPrefHeight(50);
+		Button open = new Button("Open existing...");
+		open.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+		open.setPrefHeight(50);
+		VBox buttons = new VBox(startNew, open);
+		VBox.setVgrow(open, Priority.ALWAYS);
+		VBox.setVgrow(startNew, Priority.ALWAYS);
+		BorderPane root = new BorderPane(buttons);
+		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
