0b6f8fc0d7ea1730d336ebe7415e9fc02386a44c
Malfeasant <mischa.sumrall@gmail.com>
2018-04-01 08:27:31 -0700

    Squash a warning

--- src/us/malfeasant/spock/plumbing/LogicException.java
+++ src/us/malfeasant/spock/plumbing/LogicException.java
@@ -1,6 +1,8 @@
 package us.malfeasant.spock.plumbing;
 
 public class LogicException extends Exception {
+	private static final long serialVersionUID = -6029622701784091810L;
+
 	public LogicException(String message) {
 		super(message);
 	}
